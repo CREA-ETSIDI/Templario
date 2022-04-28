8bf2401b1bbedd21a8686933b5c37b9b489d7542
orgynmobs <62205153+orgynmobs@users.noreply.github.com>
2022-04-28 19:03:13 +0200

    cambios al control puente H
    cambio de los pines para ajustar la funcion analog write
    ajuste del map para todo el rango

---
+++ Radio_FREQ_receptor_Templario/Radio_FREQ_receptor.ino
@@ -0,0 +1,179 @@
+#include "nRF24L01.h"
+#include <RF24.h>
+#include "RF24_config.h"
+#include <SPI.h>
+#include <ServoTimer2.h>  // the servo library
+
+#define pinPHA1 5
+#define pinPHA2 4
+#define pinPHB1 6
+#define pinPHB2 7
+
+ServoTimer2 rodillo;
+int JoyIzq_map;
+int JoyDer_map;
+
+typedef struct  {
+int JoyIzq;
+int JoyDer; 
+bool boton1;
+bool boton2;
+}InfoMando;
+
+unsigned long UltimaConexion = 0;
+int packetCounts[10];
+int packetCountIndex = 0;
+int packetCountTotal = 0;
+//char ppsBuf[16];
+
+ unsigned long packetsRead = 0;
+unsigned long lastScreenUpdate = 0;
+unsigned long lastRecvTime = 0;
+unsigned long drops = 0;
+
+ 
+const int pinCE = 9;
+const int pinCSN = 10;
+RF24 radio(pinCE, pinCSN);
+ 
+// Single radio pipe address for the 2 nodes to communicate.
+//const uint64_t pipe = 0xE8E8F0F0E1LL;
+ byte direccion[5] ={'c','a','n','a','l'};
+char data[16];
+ InfoMando Mimando;
+
+int motor1(int,int);
+int motor2(int,int);
+bool arma(int);
+
+
+
+void updateScreen()
+{  
+  unsigned long now = millis();
+  if ( now - lastScreenUpdate < 100 )
+    return;
+    
+  // moving average over 1 second
+  packetCountTotal -= packetCounts[packetCountIndex];
+  packetCounts[packetCountIndex] = packetsRead;
+  packetCountTotal += packetsRead;
+
+  packetCountIndex = (packetCountIndex + 1) % 10; // cada segundo llena el bufer y hace ciclos de 1 seg
+  packetsRead = 0;                  //hace media y calcula si se pierden paquetes o no 
+
+
+
+  Serial.print(" lectura Joystick izq con map: ");
+Serial.println(JoyIzq_map);
+Serial.print(" lectura Joystick der con map : ");
+Serial.println(JoyDer_map);
+Serial.print(" lectura botones: ");
+Serial.print(Mimando.boton1);
+Serial.println(Mimando.boton2);
+Serial.print(" paquetes: ");
+Serial.println(packetCountTotal);
+  //sprintf(ppsBuf, "PPS: %d", packetCountTotal);  //pps = paquetes por segundo
+  
+  lastScreenUpdate = millis();  
+}
+
+
+void setup(void)
+{
+   Serial.begin(115200);
+   radio.begin();
+   radio.setDataRate(RF24_250KBPS);
+    radio.setAutoAck(0);
+   radio.openReadingPipe(0,direccion);
+   radio.startListening();
+
+   memset( packetCounts, 0, sizeof(packetCounts) );
+ pinMode(pinPHA1,OUTPUT);
+ pinMode(pinPHB1,OUTPUT);
+}
+ 
+void loop(void)
+{
+
+  int velA;
+  int velB;
+  //Variables control
+  int c1;
+  int c2;
+  int c3;
+  
+
+
+
+  
+   if (radio.available())
+   {
+    radio.read(&Mimando, sizeof  Mimando);
+       //radio.read(&data, sizeof data); 
+//Serial.print(" lectura Joystick izq: ");
+//Serial.println(Mimando.JoyIzq);
+//Serial.print(" lectura Joystick der: ");
+//Serial.println(Mimando.JoyDer);
+//Serial.print(" lectura botones: ");
+//Serial.println(Mimando.Botones);
+ packetsRead++;
+  UltimaConexion = millis();
+//Serial.print(Mimando.boton1);
+//Serial.println(Mimando.boton2);
+JoyIzq_map = map(Mimando.JoyIzq,0,1023,0,255);
+JoyDer_map= map(Mimando.JoyDer,0,1023,0,255);
+
+
+ c1=motor1(JoyIzq_map,Mimando.boton1);
+      c2=motor2(JoyDer_map,Mimando.boton1);
+      c3=arma(Mimando.boton2);
+
+  
+   }
+   updateScreen();
+ 
+   
+   if(((millis()- UltimaConexion) > 3000) ){
+  Mimando.JoyIzq = 0;
+  Mimando.JoyDer = 0;
+  Mimando.boton1= 0;
+  Mimando.boton2= 0;
+UltimaConexion = millis();
+
+radio.begin();
+    radio.setAutoAck(0);
+ radio.setDataRate(RF24_250KBPS);
+   radio.openReadingPipe(0,direccion);
+   radio.startListening();
+  
+}
+
+
+}
+
+
+int motor1(int a, int c){
+    analogWrite(pinPHA1,a);
+    digitalWrite(pinPHA2,c); 
+  return a;
+}
+
+int motor2(int b,int c){
+    analogWrite(pinPHB1,b);
+    digitalWrite(pinPHB2,c); 
+  return b;
+}
+
+bool arma(int c){
+  //1 encendido, 0 apagado
+  rodillo.attach(3);
+  if(c==0){
+    rodillo.write(1200);
+    return 0;
+  }
+  else{
+    rodillo.write(0);
+    return 1;
+  }
+}
